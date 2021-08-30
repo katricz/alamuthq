45ef3a0f69937eeb2a14238250473903b5feae73
Katricz <alamuthq@gmail.com>
2021-08-30 12:21:52 -0300

    Working on crypt page

---
+++ moch/krcgCrypt.js
@@ -0,0 +1,44 @@
+//   https://api.krcg.org/card_search Post='crypt'
+
+
+const krcgCrypt = [
+  "Aabbt Kindred",
+  "Aaron Bathurst",
+  "Aaron Duggan, Cameron's Toady",
+  "Abd al-Rashid",
+  "Abdelsobek",
+  "Abebe",
+  "Abid bin Haji, Scholar of Mirrors",
+  "Abiku",
+  "Abu Nuwasi",
+  "Accorri Giovanni",
+  "Adana de Sforza",
+  "Adelaide Davis",
+  "Adhiambo",
+  "Adonai",
+  "Adri, The Seeker",
+  "Adrianne",
+  "Aeron",
+  "Afifa, The Herald",
+  "Agaitas, The Scholar of Antiquities",
+  "Agatha",
+  "Agrippina",
+  "Agru Kabera",
+  "Ahmose Chambers",
+  "Aidan Lyle",
+  "Aimee Laroux",
+  "Aimery Methuin",
+  "Aisata Swanou",
+  "Aisha",
+  "Aisha az-Zahra",
+  "Aisling Sturbridge",
+  "Aiyana, The Wolfcatcher",
+  "Akram",
+  "Aksinya Daclau",
+  "Al-Ashrad, Amr of Alamut",
+  "Al-Ashrad, Amr of Alamut (ADV)",
+  "Al-Muntaquim, The Avenger"
+]
+
+
+export default krcgCrypt
--- pages/crypt.js
+++ pages/crypt.js
@@ -1,12 +1,75 @@
+import React from 'react';
+import { FixedSizeList } from 'react-window';
 
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import { Container } from '@material-ui/core';
+import krcgCrypt from '../moch/krcgCrypt';
 
-function Crypt() {
+// Icones do awesomefont 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+
+/*
+export default function Crypt() {
     return (
         <div>
-            Welcome to AlamutHQ 
-            <a href='/'> HomePage </a>
+            <Container>
+                <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
+                    {krcgCrypt.map((cardName) => (
+                        <CryptList to="/" primary={cardName} icon={<FontAwesomeIcon icon='home' />} />
+                    ))}
+                </FixedSizeList>
+            </Container>
         </div>
+    );
+}
+
+function CryptList(props) {
+    const { icon, primary, to, newtab } = props;
+  
+    return (
+        <ListItem button component={"a"} href={to} target={newtab}>
+            {icon ? <ListItemIcon> {icon} </ListItemIcon> : null}
+            <ListItemText primary={primary} />
+        </ListItem>
+        );
+}
+*/
+
+
+export default function Crypt() {
+    return (
+        <div className="table-responsive">
+
+            <Container>
+                <FixedSizeList
+                    height={400} itemSize={46} itemCount={200}
+                    children={CryptList}
+                />
+            </Container>
+        </div>
+    );
+}
+
+
+function CryptList() {
+    return (
+        <div>
+            {krcgCrypt.map((cardName) => (
+                <ListItem button key={cardName} component={"a"} to="/" icon={<FontAwesomeIcon icon='home' />} >
+                    <ListItemAvatar>
+                        <Avatar alt="Remy Sharp" src="https://static.krcg.org/card/aabbtkindred.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText primary={cardName} />
+                </ListItem>
+            ))
+            }
+        </div >
     )
 }
 
-export default Crypt
