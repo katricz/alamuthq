a7b4d40001823f216e0075ed6ef02da0a3312705
Katricz <alamuthq@gmail.com>
2021-09-20 18:10:44 -0300

    Other Pages

--- pages/decks.js
+++ pages/decks.js
@@ -1,4 +1,4 @@
-
+import { Container } from "@material-ui/core";
 
 function Decks() {
     return (
--- pages/inventory.js
+++ pages/inventory.js
@@ -1,4 +1,4 @@
-
+import { Container } from "@material-ui/core";
 
 function Inventory() {
     return (
